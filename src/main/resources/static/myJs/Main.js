278abc3f40e394262458edae9b062213dbfc3fc1
SmuragaAA <SmuragaAA@belpost.by>
2023-03-02 17:13:35 +0300

    dragscroll bar finished

--- src/main/resources/static/myJs/Main.js
+++ src/main/resources/static/myJs/Main.js
@@ -1,39 +1,71 @@
-$(document).ready(function(){
-    $('#nav-icon').click(function(){
+$(document).ready(function () {
+    $('#nav-icon').click(function () {
         $(this).toggleClass('open');
         $('#sidebar').toggleClass('open');
     });
 });
-$(document).ready(function() {
-    let pointerFrom = 0;
-    let elementFrom = 0;
 
-    const scrollable = document.querySelector('.wrapper');
+//-----------ДЛЯ ПЕРЕТАСКИВАНИЯ СКРОЛБАРА-------------------
+// $(document).ready(function () {
+//     let pointerFrom = 0;
+//     let elementFrom = 0;
+//
+//     const scrollable = document.querySelector('.wrapper');
+//
+//     const onDrag = (event) => {
+//         if (event.pointerType == 'mouse') {
+//             scrollable.scrollLeft = elementFrom - event.clientX + pointerFrom;
+//         }
+//     };
+//
+//     scrollable.addEventListener('pointerdown', (event) => {
+//         if (event.pointerType == 'mouse') {
+//             pointerDown = true;
+//             // Set the position where the mouse is starting to drag from.
+//             pointerFrom = event.clientX;
+//             // Set the position of the element is scrolled from.
+//             elementFrom = scrollable.scrollLeft;
+//             // React on pointer move.
+//             document.addEventListener('pointermove', onDrag);
+//         }
+//     });
+//
+// // Stop reacting on pointer move when pointer is no longer clicked.
+//     document.addEventListener('pointerup', (event) => {
+//         // Ensure we only do this for pointers that don't have native
+//         // drag-scrolling behavior.
+//         if (event.pointerType == 'mouse') {
+//             document.removeEventListener('pointermove', onDrag);
+//         }
+//     });
+// });
 
-    const onDrag = (event) => {
-        if (event.pointerType == 'mouse') {
-            scrollable.scrollLeft = elementFrom - event.clientX + pointerFrom;
-        }
+//---------------------------------------------------------------------------
+$(document).ready(function () {
+    console.log(window.innerWidth)
+    const buttonRight = document.getElementById('slideRight');
+    const buttonLeft = document.getElementById('slideLeft');
+    var container = document.getElementById('scroller');
+
+    buttonRight.onclick = function () {
+        sideScroll(container,'right',15,350,20);
     };
-
-    scrollable.addEventListener('pointerdown', (event) => {
-        if (event.pointerType == 'mouse') {
-            pointerDown = true;
-            // Set the position where the mouse is starting to drag from.
-            pointerFrom = event.clientX;
-            // Set the position of the element is scrolled from.
-            elementFrom = scrollable.scrollLeft;
-            // React on pointer move.
-            document.addEventListener('pointermove', onDrag);
-        }
-    });
-
-// Stop reacting on pointer move when pointer is no longer clicked.
-    document.addEventListener('pointerup', (event) => {
-        // Ensure we only do this for pointers that don't have native
-        // drag-scrolling behavior.
-        if (event.pointerType == 'mouse') {
-            document.removeEventListener('pointermove', onDrag);
-        }
-    });
+    buttonLeft.onclick = function () {
+        sideScroll(container,'left',15,350,20);
+    };
 });
+
+function sideScroll(element,direction,speed,distance,step){
+    var scrollAmount = 0;
+    var slideTimer = setInterval(function(){
+        if(direction == 'left'){
+            element.scrollLeft -= step;
+        } else {
+            element.scrollLeft += step;
+        }
+        scrollAmount += step;
+        if(scrollAmount >= distance){
+            window.clearInterval(slideTimer);
+        }
+    }, speed);
+}
