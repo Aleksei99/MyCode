980007e06bad4c188ca0297791e83fca9d4002ac
SmuragaAA <SmuragaAA@belpost.by>
2023-03-13 17:11:14 +0300

    added section save

--- src/main/java/com/smuraha/mycode/controller/AdminController.java
+++ src/main/java/com/smuraha/mycode/controller/AdminController.java
@@ -1,12 +1,15 @@
 package com.smuraha.mycode.controller;
 
 import com.smuraha.mycode.dao.model.Image;
+import com.smuraha.mycode.dao.model.Section;
 import com.smuraha.mycode.dao.repo.ImageRepository;
+import com.smuraha.mycode.dao.repo.SectionRepository;
 import jakarta.servlet.ServletOutputStream;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,22 +25,27 @@ import java.util.UUID;
 public class AdminController {
 
     private final ImageRepository imageRepository;
+    private final SectionRepository sectionRepository;
 
     @GetMapping("/admin")
-    public String getAdminPage(){
+    public String getAdminPage(Model model){
+        model.addAttribute("sections",sectionRepository.findAll());
         return "admin";
     }
 
-    @PostMapping("/uploadImage")
-    public void uploadImage(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    @PostMapping("/addSection")
+    public String uploadImage(HttpServletRequest request) throws IOException {
         String uuid = UUID.randomUUID().toString();
         String fileName = uuid + ".png";
         Image image = new Image(fileName,"image/png",((StandardMultipartHttpServletRequest) request).getFile("image").getBytes());
-        imageRepository.save(image);
-        String imageUrl = request.getContextPath()+"/getImage?name="+fileName;
-        response.setContentType("text/plain");
-        response.setCharacterEncoding("UTF-8");
-        response.getWriter().write(imageUrl);
+        imageRepository.saveAndFlush(image);
+        Section section = new Section(request.getParameter("name"),
+                request.getParameter("sectionHeader"),
+                image,
+                Integer.parseInt(request.getParameter("width")),
+                Integer.parseInt(request.getParameter("height")));
+        sectionRepository.save(section);
+        return "redirect:/admin";
     }
 
     @GetMapping("/getImage")
--- src/main/java/com/smuraha/mycode/dao/repo/ImageRepository.java
+++ src/main/java/com/smuraha/mycode/dao/repo/ImageRepository.java
@@ -1,8 +1,9 @@
 package com.smuraha.mycode.dao.repo;
 
 import com.smuraha.mycode.dao.model.Image;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ImageRepository extends CrudRepository<Image, Long> {
+public interface ImageRepository extends JpaRepository<Image, Long> {
     Image getImageByName(String name);
 }
